a70a0017493ae9d9d350714e461dc2a0e3ee383d
javster101 <javster101@outlook.com>
2015-09-12 10:55:50 -0400

    Added placeholder Event handling of classes

---
+++ OpenGG Project/src/com/opengg/core/input/KeyboardEventHandler.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opengg.core.input;
+
+import org.lwjgl.glfw.GLFWKeyCallback;
+
+/**
+ *
+ * @author Javier
+ */
+public class KeyboardEventHandler  extends GLFWKeyCallback{
+
+    @Override
+    public void invoke(long window, int key, int scancode, int action, int mods) {
+        
+    }
+    
+}
