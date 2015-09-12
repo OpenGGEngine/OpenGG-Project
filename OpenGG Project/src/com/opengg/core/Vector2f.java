bc24470d1a998a3c3b9414055e8e8c224f42fdca
javster101 <javster101@outlook.com>
2015-09-12 19:38:14 -0400

    Added Mouse positional support, 2d vector, and example in test class

---
+++ OpenGG Project/src/com/opengg/core/Vector2f.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opengg.core;
+
+/**
+ *
+ * @author Javier
+ */
+public class Vector2f {
+    public float x;
+    public float y;
+
+    /**
+     * Creates a default 2d vector with all values set to 0.
+     */
+    public Vector2f() {
+        this.x = 0f;
+        this.y = 0f;
+    }
+    
+    /**
+     * Creates a vector based off of 2 points.
+     * @param x
+     * @param y
+     */
+    public Vector2f(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+    /**
+     * Creates a new 2d vector based off another.
+     * @param v Vector to be copied
+     */
+    public Vector2f(Vector2f v){
+        this.x = v.x;
+        this.y = v.y;
+    }
+    /**
+     * Gets the distance between two vectors.
+     * @param v
+     * @return 
+     */
+    
+    public float getDistance(Vector2f v){
+        return (float) Math.sqrt((this.x * v.x)+(this.y * v.y));  
+       
+    }
+    
+    @Override
+    public String toString(){
+        String s = String.valueOf(this.x) + ", " + String.valueOf(this.y);
+        return s;
+    }
+}
--- OpenGG Project/src/com/opengg/core/test/Test.java
+++ OpenGG Project/src/com/opengg/core/test/Test.java
@@ -6,13 +6,12 @@
 package com.opengg.core.test;
 
 import com.opengg.core.window.Window;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 import org.lwjgl.opengl.GL;
 import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.opengl.GLContext;
-
+import com.opengg.core.input.KeyBoardHandler;
+import com.opengg.core.input.MousePosHandler;
+import static org.lwjgl.glfw.GLFW.*;
 /**
  *
  * @author 19coindreauj
@@ -22,25 +21,36 @@ public class Test {
     static long window;
     
     public static void main(String args[]){
+        
         Window win = new Window();
         try {
             window = win.init(1280, 720, "Test", false);
         } catch (Exception ex){ex.printStackTrace();}
         
         loop();
+        
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+
     }
     
-    
     static void loop(){
-         GL.setCurrent(GLContext.createFromCurrent()); 
+        MousePosHandler.setup(window);
+        
+        GL.setCurrent(GLContext.createFromCurrent()); 
         while ( glfwWindowShouldClose(window) == GL_FALSE ) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
             
-            glfwSwapBuffers(window); // swap the color buffers
+            //System.out.println(MousePosHandler.getPos());
             
+            if(KeyBoardHandler.isKeyDown(GLFW_KEY_W)){
+                System.out.println(MousePosHandler.getPos());
+            }
             // Poll for window events. The key callback above will only be
             // invoked during this call.
+            
+            glfwSwapBuffers(window);
             glfwPollEvents();
         }
     }
