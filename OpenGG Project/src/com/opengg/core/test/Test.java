0edcfe52aedf7673366784db7bc71098fa03d4a9
warren <warren@Warrenroom.home>
2015-09-12 22:32:06 -0500

    Test Update Loop Implemented

--- OpenGG Project/src/com/opengg/core/test/Test.java
+++ OpenGG Project/src/com/opengg/core/test/Test.java
@@ -46,10 +46,11 @@ public class Test implements KeyboardListener{
         while ( glfwWindowShouldClose(window) == GL_FALSE ) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
             
-            
+              win.update();
             
             glfwSwapBuffers(window);
             glfwPollEvents();
+          
         }
     }
 
