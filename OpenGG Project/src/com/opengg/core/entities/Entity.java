cd2838d84dc5ff3da7881d7c62054f6420c38117
ethachu19 <ethan.mak100@gmail.com>
2015-09-16 23:09:28 -0400

    Started with the physics of entities

--- OpenGG Project/src/com/opengg/core/entities/Entity.java
+++ OpenGG Project/src/com/opengg/core/entities/Entity.java
@@ -10,22 +10,35 @@ package com.opengg.core.entities;
  * @author ethachu19
  */
 public class Entity {
-
+    
+    static public int entityCount = 0;
     public float x;
     public float y;
     public float z;
-    public float velocity;
+    public float velocity = 0f;
     public float acceleration;
+    public boolean ground;
+    public float mass;
+    public float volume;
 
     /**
-     * Creates a default entity with all values set to 0.
+     * Creates a default entity with all values set to 0 and on ground.
+     * @throws java.lang.IllegalAccessException
      */
-    public Entity() {
+    public Entity() throws IllegalAccessException {
+        if(entityCount >= 40)
+        {
+            throw new IllegalAccessException("Entity count is too high");
+        }
         this.x = 0f;
         this.y = 0f;
         this.z = 0f;
         this.velocity = 0f;
         this.acceleration = 0f;
+        this.ground = true;
+        this.volume = 0f;
+        this.mass = 0f;
+        Entity.entityCount++;
     }
 
     /**
@@ -35,41 +48,68 @@ public class Entity {
      * @param y
      * @param z
      * @param velocity
-     * @param acceleration
+     * @param force
+     * @param ground
+     * @param mass
+     * @param volume
+     * @throws java.lang.IllegalAccessException
      */
-    public Entity(float x, float y, float z, float velocity, float acceleration) {
+    public Entity(float x, float y, float z, float force, boolean ground, float mass, float volume) throws IllegalAccessException {
+        if(entityCount >= 40)
+        {
+            throw new IllegalAccessException("Entity count is too high");
+        }
         this.x = x;
         this.y = y;
         this.z = z;
-        this.velocity = velocity;
-        this.acceleration = acceleration;
+        this.acceleration = force/mass;
+        this.ground = ground;
+        this.volume = volume;
+        this.mass = mass;
+        Entity.entityCount++;
     }
 
     /**
      * Creates a new vector based off another.
      *
      * @param v Entity to be copied
+     * @throws java.lang.IllegalAccessException
      */
-    public Entity(Entity v) {
+    public Entity(Entity v) throws IllegalAccessException {
+        if(entityCount >= 40)
+        {
+            throw new IllegalAccessException("Entity count is too high");
+        }
         this.x = v.x;
         this.y = v.y;
         this.z = v.z;
         this.velocity = v.velocity;
         this.acceleration = v.acceleration;
+        this.ground = v.ground;
+        this.volume = v.volume;
+        this.mass = v.mass;
+        Entity.entityCount++;
     }
 
     /**
      * Creates new Entity off of old entity with different movement speed
      * 
-     * @param velocity
-     * @param acceleration
+     * @param force
      * @param v Entity to be copied
+     * @throws java.lang.IllegalAccessException
      */
-    public Entity(float velocity, float acceleration, Entity v) {
-        this.velocity = velocity;
-        this.acceleration = acceleration;
+    public Entity(float force, Entity v) throws IllegalAccessException {
+        if(entityCount >= 40)
+        {
+            throw new IllegalAccessException("Entity count is too high");
+        }
+        this.acceleration = force/v.mass;
         this.x = v.x;
         this.y = v.y;
         this.z = v.z;
+        this.ground = v.ground;
+        this.volume = v.volume;
+        this.mass = v.mass;
+        Entity.entityCount++;
     }
 }
---
+++ OpenGG Project/src/com/opengg/core/physics/collisionDetection.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opengg.core.physics;
+
+import com.opengg.core.entities.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ethachu19
+ */
+public class collisionDetection {
+    List<Entity> list = new ArrayList<>();
+    
+    public void addEntity(Entity entity)
+    {
+        list.add(entity);
+    }
+    
+    public void checkforNulls()
+    {
+        for(int i = 0; i<list.size(); i++)
+        {
+            if(list.get(i) == null)
+            {
+                list.remove(i);
+            }
+        }
+    }
+    
+    public boolean areColliding(int x, int y)
+    {
+        if(list.get(x) == null || list.get(y) == null)
+        {
+            return false;
+        }
+        
+        
+        
+        return true;
+    }
+    
+}
