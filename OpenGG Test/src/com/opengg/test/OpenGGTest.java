b3e0399683ba94d78e523b95012fc4bc47a7727b
javster101 <javster101@outlook.com>
2015-09-19 13:15:11 -0400

    Added Scrub texture to square, added linking to remove redundant vectors, and moved shaders to separate class

--- OpenGG Test/src/com/opengg/test/OpenGGTest.java
+++ OpenGG Test/src/com/opengg/test/OpenGGTest.java
@@ -1,29 +1,26 @@
 package com.opengg.test;
 import com.opengg.core.Matrix4f;
+import com.opengg.core.Texture.Texture;
 import com.opengg.core.input.KeyboardEventHandler;
 import com.opengg.core.input.KeyboardListener;
-import com.opengg.core.input.MousePosHandler;
 import com.opengg.core.render.VertexArrayObject;
 import com.opengg.core.render.VertexBufferObject;
 import com.opengg.core.shader.Shader;
 import com.opengg.core.shader.ShaderProgram;
 import com.opengg.core.util.Time;
-import org.lwjgl.Sys;
-import org.lwjgl.opengl.*;
  
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import com.opengg.core.window.*;
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
-import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
-import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
-import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
 public class OpenGGTest implements KeyboardListener{
  
     // We need to strongly reference callback instances.
@@ -31,44 +28,21 @@ public class OpenGGTest implements KeyboardListener{
     Window win = new Window();
     
     boolean draw = true;
+    int vertAmount = 0;
+    int triangleAmount;
     
+    private Shader vertexTex;
+    private Shader fragmentTex;
+    
+    {
+        triangleAmount = vertAmount / 3;
+    }
     int uniView;
     public float x,y,z;
     public float xm = 0, ym=0, zm=0;
     private float angle = 0f;
     private float anglePerSecond = 10f;
-    
-    private final CharSequence vertexSource
-            = "#version 150 core\n"
-            + "\n"
-            + "in vec3 position;\n"
-            + "in vec3 color;\n"
-            + "\n"
-            + "out vec3 vertexColor;\n"
-            + "\n"
-            + "uniform mat4 model;\n"
-            + "uniform mat4 view;\n"
-            + "uniform mat4 projection;\n"
-            + "\n"
-            + "void main() {\n"
-            + "    vertexColor = color;\n"
-            + "    mat4 mvp = projection * view * model;\n"
-            + "    gl_Position = mvp * vec4(position, 1.0);\n"
-            + "}";
-    private final CharSequence fragmentSource
-            = "#version 150 core\n"
-            + "\n"
-            + "in vec3 vertexColor;\n"
-            + "\n"
-            + "out vec4 fragColor;\n"
-            + "\n"
-            + "void main() {\n"
-            + "    fragColor = vec4(vertexColor, 1.0);\n"
-            + "}";
-    
-    /**
-     * @param args the command line arguments
-     */
+
     double rot1 = 0;
     boolean backwards = false;
     
@@ -79,9 +53,16 @@ public class OpenGGTest implements KeyboardListener{
     private VertexBufferObject vbo;
     Shader vertexShader, fragmentShader;
     private ShaderProgram program;
+    private ShaderProgram program2;
     private int uniModel;
     private float previousAngle;
     
+    Texture t1 = new Texture();
+    
+    float speed = 0.3f;
+    
+    FloatBuffer vertices;
+    FloatBuffer vertices2;
     public OpenGGTest(){
         Window w = new Window();
         long window = 1;
@@ -92,17 +73,17 @@ public class OpenGGTest implements KeyboardListener{
         } catch (Exception ex) {
             Logger.getLogger(OpenGGTest.class.getName()).log(Level.SEVERE, null, ex);
         }
-        GL.setCurrent(GLContext.createFromCurrent());
+        
         //e.enter();
         float delta;
         setup();
-        while(GLFW.glfwWindowShouldClose(window) == GL_FALSE){
-            //e.render(1);
+        while(!win.shouldClose((int) window)){
+            
+            startFrame();
             delta = Time.getDelta();
             update(delta / 1000);
             render(rot1);
-            glfwSwapBuffers(window);
-            glfwPollEvents();
+            finishFrame((int) window);
         }
         exit();
     }
@@ -110,56 +91,85 @@ public class OpenGGTest implements KeyboardListener{
         /* Generate Vertex Array Object */
         vao = new VertexArrayObject();
         vao.bind();
+        
+        t1.getBMP("C:/res/tex1.png");
+        
+        ByteBuffer texBuffer = t1.getData();
+        
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 128, 128, 0, GL_RGBA, GL_UNSIGNED_BYTE, texBuffer);
+        
 
-        /* Vertex data */
-        FloatBuffer vertices = BufferUtils.createFloatBuffer(6 * 6);
-        vertices.put(0f).put(0.9f).put(-2f).put(1f).put(0f).put(1f);
-        vertices.put(0.9f).put(0.4f).put(-2f).put(0f).put(1f).put(1f);
-        vertices.put(0.9f).put(0.8f).put(0f).put(0f).put(1f).put(1f);
-        vertices.put(-0.6f).put(-0.4f).put(-1f).put(1f).put(0f).put(0f);
-        vertices.put(0.6f).put(-0.4f).put(-1f).put(0f).put(1f).put(0f);
-        vertices.put(0f).put(0.5f).put(-1f).put(0f).put(0f).put(1f);
+        
+        vertices = BufferUtils.createFloatBuffer(8 * 8);
+        vertices.put(-2).put(-2).put(0).put(0f).put(1f).put(0f).put(0f).put(0f); 
+        vertices.put(2).put(-2).put(0).put(0f).put(0f).put(1f).put(1f).put(0f);
+        vertices.put(2).put(2).put(0).put(1f).put(0f).put(0f).put(1f).put(1f);
+        vertices.put(-2).put(2).put(0).put(0f).put(0f).put(0f).put(0f).put(1f);
         
         vertices.flip();
 
-        /* Generate Vertex Buffer Object */
+        
+        int ebo = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        
+        IntBuffer elements = BufferUtils.createIntBuffer(2 * 3);
+        elements.put(0).put(1).put(2);
+        elements.put(2).put(3).put(0);
+//        elements.put(4).put(5).put(6);
+//        elements.put(6).put(7).put(4);
+        elements.flip();
+        
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements, GL_STATIC_DRAW);
+        
+        
+        
         vbo = new VertexBufferObject();
         vbo.bind(GL_ARRAY_BUFFER);
         vbo.uploadData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
         
-         /* Load shaders */
-        vertexShader= new Shader(GL_VERTEX_SHADER, vertexSource); 
-        fragmentShader = new Shader(GL_FRAGMENT_SHADER, fragmentSource); 
 
+         /* Load shaders */
+        vertexShader= new Shader(GL_VERTEX_SHADER, Shaders.vertexSource); 
+        fragmentShader = new Shader(GL_FRAGMENT_SHADER, Shaders.fragmentSource); 
+        vertexTex= new Shader(GL_VERTEX_SHADER, Shaders.vertexTex); 
+        fragmentTex = new Shader(GL_FRAGMENT_SHADER, Shaders.fragmentTex); 
 
         /* Create shader program */
         program = new ShaderProgram();
-        program.attachShader(vertexShader);   
-        program.attachShader(fragmentShader);  
+        program.attachShader(vertexTex);   
+        program.attachShader(fragmentTex);  
         program.bindFragmentDataLocation(0, "fragColor");
         program.link();
         program.use();
         program.checkStatus();
+        
+        
         specifyVertexAttributes();
+       
 
-        /* Get uniform location for the model matrix */
-        uniModel = program.getUniformLocation("model");
-
-        /* Set view matrix to identity matrix */
+        /* Set shader variables */
         Matrix4f view = new Matrix4f();
+        
+        uniModel = program.getUniformLocation("model");
+        
         uniView = program.getUniformLocation("view");
         program.setUniform(uniView, view);
-
+        
+        int uniTex = program.getUniformLocation("texImage");
+        program.setUniform(uniTex, 0);
 
         float ratio = win.getRatio();
         
         //Matrix4f projection = Matrix4f.orthographic(-ratio, ratio, -1f, 1f, -4f, 4f);
-        Matrix4f projection = Matrix4f.perspective(100, 640/480, 0.1f, 10);
-        int uniProjection = program.getUniformLocation("projection");
-        program.setUniform(uniProjection, projection);
+        ViewUtil.setPerspective(100, ratio, 0.1f, 300, program);
         vao.bind();
         program.use();
+        
+        glEnable(GL_DEPTH_TEST);
 
+        glDepthFunc(GL_LESS);
+
+        
     }
     
     
@@ -167,12 +177,16 @@ public class OpenGGTest implements KeyboardListener{
         /* Specify Vertex Pointer */
         int posAttrib = program.getAttributeLocation("position");
         program.enableVertexAttribute(posAttrib);
-        program.pointVertexAttribute(posAttrib, 3, 6 * Float.BYTES, 0);
+        program.pointVertexAttribute(posAttrib, 3, 8 * Float.BYTES, 0);
 
         /* Specify Color Pointer */
         int colAttrib = program.getAttributeLocation("color");
         program.enableVertexAttribute(colAttrib);
-        program.pointVertexAttribute(colAttrib, 3, 6 * Float.BYTES, 3 * Float.BYTES);
+        program.pointVertexAttribute(colAttrib, 3, 8 * Float.BYTES, 3 * Float.BYTES);
+        
+        int texAttrib = program.getAttributeLocation("texcoord");
+        program.enableVertexAttribute(texAttrib);
+        program.pointVertexAttribute(texAttrib, 2, 8 * Float.BYTES, 6 * Float.BYTES);
     }
     
     public void exit() {
@@ -184,7 +198,7 @@ public class OpenGGTest implements KeyboardListener{
     }
     
     public void render(double alpha) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
 
         float lerpAngle;
         if(rot1 != 0){
@@ -195,24 +209,24 @@ public class OpenGGTest implements KeyboardListener{
         if(backwards){
             lerpAngle = -lerpAngle;
         }
-        
-        
-       
+
         vao.bind();
         program.use();
 
         Matrix4f model = Matrix4f.rotate(lerpAngle, 0f, 1f, 0f);
         Matrix4f move = Matrix4f.translate(x, y, z);
         //Matrix4f scale = Matrix4f.scale(0.4f, 0.4f, 0.4f);
+        Matrix4f end = move.add(model);
+        vbo.uploadData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
         
-        
-        
-        program.setUniform(uniModel, move);
+        program.setUniform(uniModel, end);
         
         if(draw){
-            glDrawArrays(GL_TRIANGLES, 0, 6);
+            glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+            
             draw = true;
         }else{
+            
             draw = true;
         }
         
@@ -228,81 +242,78 @@ public class OpenGGTest implements KeyboardListener{
 
     @Override
     public void keyPressed(int key) {
-        if(key == GLFW_KEY_D){
-            rot1 -= 0.2;
-            xm -= 0.1;
-        }
         if(key == GLFW_KEY_A){
-
-            xm += 0.1;
+            xm += speed;
 
         }
-        if(key == GLFW_KEY_S){
- 
-            zm -= 0.1;
+        if(key == GLFW_KEY_D){
+            xm -= speed;
+
         }
         if(key == GLFW_KEY_W){
+ 
+            zm += speed;
+        }
+        if(key == GLFW_KEY_S){
 
-            zm += 0.1;
+            zm -= speed;
 
         }
         if(key == GLFW.GLFW_KEY_LEFT_SHIFT){
 
-            ym += 0.1;
+            ym -= speed;
         }
         if(key == GLFW_KEY_LEFT_CONTROL){
 
-            ym -= 0.1;
+            ym += speed;
 
         }
-        if(key == GLFW_KEY_L){
-
-            Matrix4f projection = Matrix4f.perspective(100, 640/480, 0.1f, 10);
-            int uniProjection = program.getUniformLocation("projection");
-            program.setUniform(uniProjection, projection);
-
+        if(key == GLFW_KEY_Q){
+            rot1 += 0.6;
+            
         }
-        if(key == GLFW_KEY_P){
+        if(key == GLFW_KEY_E){
+            rot1 -= 0.6;
             
-            float ratio = win.getRatio();
-            
-            Matrix4f projection = Matrix4f.orthographic(-ratio, ratio, -1f, 1f, -4f, 4f);
-            int uniProjection = program.getUniformLocation("projection");
-            program.setUniform(uniProjection, projection);
-
         }
     }
 
     @Override
     public void keyReleased(int key) {
-        if(key == GLFW_KEY_D){
-            rot1 -= 0.2;
-            xm += 0.1;
-            backwards = false;
-        }
         if(key == GLFW_KEY_A){
-            rot1 -= 0.2;
-            xm -= 0.1;
-            backwards = false;
+
+            xm -= speed;
         }
-        if(key == GLFW_KEY_S){
- 
-            zm += 0.1;
+        if(key == GLFW_KEY_D){
+
+            xm += speed;
+
         }
         if(key == GLFW_KEY_W){
+ 
+            zm -= speed;
+        }
+        if(key == GLFW_KEY_S){
 
-            zm -= 0.1;
+            zm += speed;
 
         }
         if(key == GLFW.GLFW_KEY_LEFT_SHIFT){
 
-            ym -= 0.1;
+            ym += speed;
         }
         if(key == GLFW_KEY_LEFT_CONTROL){
 
-            ym += 0.1;
+            ym -= speed;
 
         }
-        
+        if(key == GLFW_KEY_Q){
+            rot1 -= 0.6;
+            
+        }
+        if(key == GLFW_KEY_E){
+            rot1 += 0.6;
+            
+        }
     }
 }
