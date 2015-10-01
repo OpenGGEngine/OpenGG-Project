e114a1b64b7b8a14f9f4890c3b37de52d3c020f2
WX <rlaterson@gmail.com>
2015-10-01 16:49:52 -0400

    The giant commit part 31

---
+++ OpenGG Project/src/com/opengg/objparser/common/FastInt.java
@@ -0,0 +1,29 @@
+
+
+package com.opengg.objparser.common;
+
+/**
+ * Internal implementation of the {@link IFastInt}
+ * interface.
+ * 
+
+ * 
+ */
+public class FastInt implements IFastInt {
+	
+	private int value;
+	
+	public FastInt() {
+		super();
+	}
+	
+	public void set(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public int get() {
+		return value;
+	}
+
+}
