a40afee39c428d8c4529d3e4db027a1cf20d5142
WX <rlaterson@gmail.com>
2015-10-01 17:18:50 -0400

    The giant commit part 37

---
+++ OpenGG Project/src/com/opengg/exceptions/WFException.java
@@ -0,0 +1,35 @@
+
+
+package com.opengg.exceptions;
+
+import java.io.IOException;
+
+/**
+ * This is a generic exception that indicates a problem with a WaveFront 
+ * resource.
+ * <p>
+ * Instances of this or sub-classes will generally be thrown during
+ * loading or saving of WaveFront resources.
+ *
+ */
+public class WFException extends IOException {
+
+    private static final long serialVersionUID = 1L;
+
+    public WFException() {
+        super();
+    }
+
+    public WFException(String message) {
+        super(message);
+    }
+
+    public WFException(Throwable ex) {
+        super(ex);
+    }
+
+    public WFException(String message, Throwable ex) {
+        super(message, ex);
+    }
+    
+}
