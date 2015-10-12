12031873ae18ad5787d7045d8f0a05b68f39b673
WX <rlaterson@gmail.com>
2015-10-11 23:37:06 -0400

    Adds test terrain class. Is currently not functional

---
+++ OpenGG Project/src/com/opengg/core/world/Terrain.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opengg.core.world;
+
+import com.opengg.core.Vector3f;
+import com.opengg.core.objloader.parser.OBJModel;
+import com.opengg.core.objloader.parser.OBJNormal;
+import com.opengg.core.objloader.parser.OBJParser;
+import com.opengg.core.texture.Texture;
+
+/**
+ *
+ * @author Warren
+ */
+public class Terrain {
+    private static float SIZE =800;
+    private static int VERTEX_COUNT = 128;
+    private float x,z;
+    private OBJModel model;
+    private Texture texture;
+    public Terrain(int gridx,int gridz,Texture tex){
+        this.x = gridx *SIZE;
+        this.z = gridz *SIZE;
+        this.texture = tex;
+        
+    }
+   
+	public OBJModel generateTerrain(){
+		int count = VERTEX_COUNT * VERTEX_COUNT;
+		float[] vertices = new float[count * 3];
+		float[] normals = new float[count * 3];
+		float[] textureCoords = new float[count*2];
+		int[] indices = new int[6*(VERTEX_COUNT-1)*(VERTEX_COUNT-1)];
+		int vertexPointer = 0;
+		for(int i=0;i<VERTEX_COUNT;i++){
+			for(int j=0;j<VERTEX_COUNT;j++){
+				vertices[vertexPointer*3] = (float)j/((float)VERTEX_COUNT - 1) * SIZE;
+				vertices[vertexPointer*3+1] = 0;
+				vertices[vertexPointer*3+2] = (float)i/((float)VERTEX_COUNT - 1) * SIZE;
+				normals[vertexPointer*3] = 0;
+				normals[vertexPointer*3+1] = 1;
+				normals[vertexPointer*3+2] = 0;
+				textureCoords[vertexPointer*2] = (float)j/((float)VERTEX_COUNT - 1);
+				textureCoords[vertexPointer*2+1] = (float)i/((float)VERTEX_COUNT - 1);
+				vertexPointer++;
+			}
+		}
+		int pointer = 0;
+		for(int gz=0;gz<VERTEX_COUNT-1;gz++){
+			for(int gx=0;gx<VERTEX_COUNT-1;gx++){
+				int topLeft = (gz*VERTEX_COUNT)+gx;
+				int topRight = topLeft + 1;
+				int bottomLeft = ((gz+1)*VERTEX_COUNT)+gx;
+				int bottomRight = bottomLeft + 1;
+				indices[pointer++] = topLeft;
+				indices[pointer++] = bottomLeft;
+				indices[pointer++] = topRight;
+				indices[pointer++] = topRight;
+				indices[pointer++] = bottomLeft;
+				indices[pointer++] = bottomRight;
+			}
+		}
+		return model;
+	}
+}
