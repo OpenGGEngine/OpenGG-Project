a8d2f07e09b0fbb60e5dd00408d356b14f5faf6e
ethachu19 <ethan.mak100@gmail.com>
2015-10-25 20:55:17 -0400

    Players! I think

---
+++ OpenGG Project/src/com/opengg/core/world/entities/PlayerEntity.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opengg.core.world.entities;
+
+import com.opengg.core.Vector3f;
+import com.opengg.core.io.objloader.parser.OBJModel;
+import com.opengg.core.render.shader.ShaderProgram;
+import com.opengg.core.world.Camera;
+import com.opengg.core.world.entities.EntityEnums.EntityType;
+
+/**
+ *
+ * @author ethachu19
+ */
+public class PlayerEntity extends Entity{
+    
+    public Camera playerCam = new Camera(new Vector3f(), new Vector3f());
+    
+    /**
+     * Default Constructor
+     */
+    public PlayerEntity() {
+        super();
+    }
+
+    /**
+     * Makes default Player
+     *
+     * @param model Model to be bound to Entity
+     * @param type Type of Entity
+     */
+    public PlayerEntity(OBJModel model, EntityType type){
+        super(model, type);
+    }
+
+    /**
+     * Creates an player based off of 5 parameters.
+     *
+     * @param x X coordinate
+     * @param y Y coordinate
+     * @param z Z coordinate
+     * @param f Force vector
+     * @param mass Mass of Entity
+     * @param volume Volume of Entity
+     * @param type Type of entity
+     * @param model Model to be bound to entity
+     */
+    public PlayerEntity(float x, float y, float z, Vector3f f, float mass, float volume, EntityType type, OBJModel model){
+        super(x, y, z, f, mass, volume, type, model);
+    }
+
+    /**
+     * Creates a new entity based off another.
+     *
+     * @param v Entity to be copied
+     */
+    public PlayerEntity(Entity v){
+        super(v);
+    }
+}
