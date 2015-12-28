f0b064369dd6e1a1ed41d60165db8e9c132d8223
ethachu19 <ethan.mak100@gmail.com>
2015-12-28 18:04:51 -0500

    RK4 in mainloop

--- OpenGG Project/src/com/opengg/core/world/physics/MainLoop.java
+++ OpenGG Project/src/com/opengg/core/world/physics/MainLoop.java
@@ -17,42 +17,56 @@ import java.util.logging.Logger;
  *
  * @author ethachu19
  */
-public class MainLoop extends EntityBuilder{
+public class MainLoop extends EntityBuilder {
+
     private static boolean shouldClose = false;
-    
-    public static void process(){
+    private static long currentTime, newTime;
+    private static float delta, accumulator = 0f, t = 0f; 
+    private static final float dt = 0.01f;
+    public static void process() {
+        currentTime = System.currentTimeMillis();
         shouldClose = false;
-        while(!shouldClose){
+        while (!shouldClose) {
 //            print("in loop");
-            if(EntityList.size() > entityCap)
-                for(int i = 0; EntityList.size() > entityCap; ++i)
+            if (EntityList.size() > entityCap) {
+                for (int i = 0; EntityList.size() > entityCap; ++i) {
                     EntityBuilder.destroyEntity(i);
-            for(Entity collide: EntityList){
-                if(collide.updatePosition == UpdateXYZ.Movable)
-                    collide.updateXYZ();
+                }
             }
-            for (Entity collide : EntityList){
-                if(collide.collision != Collide.Collidable)
+            newTime = System.currentTimeMillis();
+            delta = (newTime - currentTime) / 1000f;
+            if (delta > 0.0f) {
+                accumulator += delta;
+                for (; accumulator >= dt; accumulator -= dt, t+= dt)
+                    for (Entity update: EntityList)
+                        update.update(t, dt);
+            }
+                
+            for (Entity collide : EntityList) {
+                if (collide.collision != Collide.Collidable) {
                     continue;
-                for(Entity collidee: EntityList){
-                    if(collide.equals(collidee) || collidee.collision == Collide.Uncollidable)
+                }
+                for (Entity collidee : EntityList) {
+                    if (collide.equals(collidee) || collidee.collision == Collide.Uncollidable) {
                         continue;
-                    if(CollisionDetection.areColliding(collide, collidee)){
-                         print("HAHA!");
-                        if(collidee.updatePosition == UpdateXYZ.Immovable){
-                           
-                            print(-collide.force.x*3/2 + " " + -collide.force.y*3/2 + " " + -collide.force.z*3/2);
-                            collide.collisionResponse(new Vector3f(-collide.force.x*3/2 , -collide.force.y*3/2 , -collide.force.z*3/2));
-                            break;
+                    }
+                    if (CollisionDetection.areColliding(collide, collidee)) {
+                        print("HAHA!");
+                        if (collidee.updatePosition == UpdateXYZ.Immovable) {
+//                           
+//                            print(-collide.force.x*3/2 + " " + -collide.force.y*3/2 + " " + -collide.force.z*3/2);
+//                            collide.collisionResponse(new Vector3f(-collide.force.x*3/2 , -collide.force.y*3/2 , -collide.force.z*3/2));
+//                            break;
                         }
-                        collide.collisionResponse(collidee.force);
-                        if(collidee.collision == Collide.Collidable)
-                            collidee.collisionResponse(collide.force);
+//                        collide.collisionResponse(collidee.force);
+                        if (collidee.collision == Collide.Collidable);
+//                            collidee.collisionResponse(collide.force);
                     }
                 }
             }
-            for (int i = 0; i < AddStack.size(); i++)
-                    EntityList.add(AddStack.get(i));
+            for (int i = 0; i < AddStack.size(); i++) {
+                EntityList.add(AddStack.get(i));
+            }
             AddStack.clear();
             try {
                 Thread.sleep(1);
@@ -61,8 +75,8 @@ public class MainLoop extends EntityBuilder{
             }
         }
     }
-    
-    public static void killProcess(){
+
+    public static void killProcess() {
         MainLoop.shouldClose = true;
     }
 }
