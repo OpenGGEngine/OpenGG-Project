b6f601ff27cc2fdd1ec52222adb7f5fd4e95ab25
Javier <i-javier.coindreau@alianzacorp.mx>
2016-12-28 01:50:00 -0500

    Natives are now loaded locally, requires copying natives folder to OpenGG-Test/dist

--- OpenGG Project/src/com/opengg/core/engine/OpenGG.java
+++ OpenGG Project/src/com/opengg/core/engine/OpenGG.java
@@ -7,7 +7,6 @@
 package com.opengg.core.engine;
 
 import com.opengg.core.exceptions.IncompatibleWindowFormatException;
-import com.opengg.core.render.shader.ShaderController;
 import com.opengg.core.render.window.GLFWWindow;
 import static com.opengg.core.render.window.RenderUtil.endFrame;
 import static com.opengg.core.render.window.RenderUtil.startFrame;
@@ -15,7 +14,11 @@ import com.opengg.core.render.window.Window;
 import com.opengg.core.render.window.WindowInfo;
 import static com.opengg.core.render.window.WindowOptions.GLFW;
 import com.opengg.core.world.World;
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -27,16 +30,42 @@ public class OpenGG implements ConsoleListener{
     public static World curworld;
     static boolean end = false;
     static boolean force = false;
+    static boolean verbose = false;
     
     public static void initialize(GGApplication app, WindowInfo windowinfo){        
-        OpenGG.app = app;
-        if(windowinfo.type == GLFW){
-            window = new GLFWWindow(windowinfo);
-        }else{
-            throw new IncompatibleWindowFormatException("Window type passed in is unknown!");
+        if(System.getProperty("os.name").contains("Windows")){
+            System.setProperty("org.lwjgl.librarypath", new File("natives\\windows").getAbsolutePath());
+        }else if(System.getProperty("os.name").contains("OS X")){
+           System.setProperty("org.lwjgl.librarypath", new File("natives\\osx").getAbsolutePath());
+        }else if(System.getProperty("os.name").contains("Linux")){
+            System.setProperty("org.lwjgl.librarypath", new File("natives\\linux").getAbsolutePath());
         }
-        initializeRenderEngine(app);
+        
+        GGConsole.log("OpenGG initialized, running on " + System.getProperty("os.name"));
+        String verb = System.getProperty("gg.verbose");
+        if(verb != null)
+            if(verb.equals("true"))
+                verbose = true;
+        
+        OpenGG.app = app;
+        if(windowinfo.type == GLFW)
+            window = new GLFWWindow(windowinfo);
+        else
+            throw new IncompatibleWindowFormatException("Window type passed in is unknown!");
+        
+        GGConsole.log("Window generation successful");
+        
+        if(System.getProperty("gg.istest") != null){
+            initializeRenderEngine(app);
+            GGConsole.log("Render engine initialized");
+            GGConsole.warning("Using test mode, external shaders will not load!");
+        }else{
+            initializeRenderEngine();
+            GGConsole.log("Render engine initialized");
+        }
+        
         initializeAudioController();
+        GGConsole.log("Audio controller initialized");
         curworld = WorldManager.getDefaultWorld();
         
         app.setup();
@@ -72,10 +101,15 @@ public class OpenGG implements ConsoleListener{
     
     private static void initializeRenderEngine(){
         if(window.getSuccessfulConstruction() && RenderEngine.initialized == false){
-            URL verts = ShaderController.class.getResource("glsl/shader.vert");
-            URL frags = ShaderController.class.getResource("glsl/shader.frag");
-            URL geoms = ShaderController.class.getResource("glsl/shader.geom");
-            RenderEngine.init(verts, frags, geoms);
+            try {
+                URL verts = new File("resources\\glsl\\shader.vert").toURI().toURL();
+                URL frags = new File("resources\\glsl\\shader.frag").toURI().toURL();
+                URL geoms = new File("resources\\glsl\\shader.geom").toURI().toURL();
+                RenderEngine.init(verts, frags, geoms);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(OpenGG.class.getName()).log(Level.SEVERE, null, ex);
+                throw new RuntimeException("Failed to load shaders!");
+            }
         }
     }
     
@@ -105,8 +139,12 @@ public class OpenGG implements ConsoleListener{
     
     private static void closeEngine(){
         RenderEngine.destroy();
+        GGConsole.log("Render engine has finalized");
         AudioController.destroy();
+        GGConsole.log("Audio controller has finalized");
         GGConsole.destroy();
+        
+        GGConsole.log("OpenGG has closed gracefully, application can now be ended");
     }
 
     @Override
