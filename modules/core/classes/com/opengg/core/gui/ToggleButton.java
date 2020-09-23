f7229a93bf6f1ffa67867ee0372dcc991c402877
Javier Coindreau <javster101@outlook.com>
2020-09-23 15:23:20 -0400

    Better UIPane repacking

--- modules/core/classes/com/opengg/core/gui/ToggleButton.java
+++ modules/core/classes/com/opengg/core/gui/ToggleButton.java
@@ -1,16 +1,29 @@
 package com.opengg.core.gui;
 
+import java.util.function.Consumer;
+
 public class ToggleButton extends UIButton {
     private UIItem toggleableContainer;
+    private Consumer<Boolean> toggleFunc = (b) ->{};
+    private boolean on = false;
 
     public ToggleButton(UIItem background) {
         super(background, () -> {});
-        this.setOnClick(() -> toggleableContainer.setEnabled(!toggleableContainer.isEnabled()));
+        this.setOnClick(() -> {
+            on = !on;
+            if(toggleableContainer != null) toggleableContainer.setEnabled(on);
+            toggleFunc.accept(on);
+        });
     }
 
-    public ToggleButton setContents(UIItem contents){
+    public ToggleButton setOnToggleContents(UIItem contents){
         this.toggleableContainer = contents;
 
+        return this;
+    }
+
+    public ToggleButton setToggleCommand(Consumer<Boolean> toggleFunc) {
+        this.toggleFunc = toggleFunc;
         return this;
     }
 }
